8610a0732c403f0007e64665069af65534da0e09
reiend <loncerasjakerei@gmail.com>
2022-07-09 08:44:51 +0800

    added eslint for linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import "@styles/scss/index.scss";
+import React from 'react';
+import '@styles/scss/index.scss';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello world from React, Typescript, scss, tailwindcss, and webpack</h1>
+      <h1>
+        Hello world from React, Typescript, scss, tailwindcss, and webpack
+      </h1>
     </div>
   );
 };
